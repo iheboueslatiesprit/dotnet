3399a4f591df42c7f51fef0eaf706402d38bbf9b
iheboueslati <iheb.oueslati@esprit.tn>
2022-05-16 23:14:15 +0100

    tp2

--- Domain/Joueur.cs
+++ Domain/Joueur.cs
@@ -8,4 +8,28 @@ namespace Domain
     {
         public string Poste { get; set; }
     }
+
+    /*
+     * TPT
+[Table("People")]
+public class Person
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+}
+
+[Table("Students")]
+public class Student : Person
+{
+    public DateTime EnrollmentDate { get; set; }
+}
+
+[Table("Teachers")]
+public class Teacher : Person
+{
+    public DateTime HireDate { get; set; }
+}
+     * 
+     */
+
 }
--- ExamenGUI/Program.cs
+++ ExamenGUI/Program.cs
@@ -10,7 +10,8 @@ namespace ExamenGUI
             //1.create data models
             //2.config data context ( dbSets ,dbName , special relations )
             //3.create DB : console nuget : Add-Migration , update-database
-                    // !!!! [ Porject de demarrage ExamenGUI , nuget COnsole project par defaut Data ]
+            // !!!! [ Porject de demarrage ExamenGUI , nuget COnsole project par defaut Data ]
+            //4.connect to db ( server name : (localdb)\mssqllocaldb )
             Console.WriteLine("Hello World!");
             ExamenContext ctx = new ExamenContext();
 
